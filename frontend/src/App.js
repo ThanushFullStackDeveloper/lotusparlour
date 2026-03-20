b1e8a4265979ca8afa5ee41cb90ef48070329266
ThanushFullStackDeveloper <114086068+ThanushFullStackDeveloper@users.noreply.github.com>
2026-03-20 12:34:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import { Toaster } from 'sonner';
 import './App.css';
 
